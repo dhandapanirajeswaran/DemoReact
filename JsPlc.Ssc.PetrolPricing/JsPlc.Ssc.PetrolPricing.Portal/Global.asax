93ca60fb7960f0f7a69f0fb16a009efa6d065f47
praveen-kumar-1 <Parveen.kumar@sainsburys.co.uk>
2015-11-19 11:08:52 +0000

    Main solution created with Portal project included

---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JsPlc.Ssc.PetrolPricing.Portal.MvcApplication" Language="C#" %>
---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JsPlc.Ssc.PetrolPricing.Portal.Startup))]
+namespace JsPlc.Ssc.PetrolPricing.Portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
