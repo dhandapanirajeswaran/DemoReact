2034721f065183724458f1ebcff4480287e79244
stevensfarkas <steven.farkas@sainsburys.co.uk>
2015-12-11 11:30:21 +0000

    removed script messing with grid layout

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Scripts/site.js
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Scripts/site.js
@@ -1,12 +1,2 @@
-﻿jQuery(function ($) {
-    var $active = $('#accordion .panel-collapse.in').prev().addClass('active');
-    $active.find('a').prepend('<i class="fa fa-minus-square"></i>');
-    $('#accordion .panel-heading').not($active).find('a').prepend('<i class="fa fa-plus-square"></i>');
-    $('#accordion').on('show.bs.collapse', function (e) {
-        $('#accordion .panel-heading.active').removeClass('active').find('.fa fa-minus-square').toggleClass('fa fa-plus-square');
-        $(e.target).prev().addClass('active').find('.fa fa-plus-square').toggleClass('fa fa-minus-square');
-    })
-});
-
-
+﻿
 
