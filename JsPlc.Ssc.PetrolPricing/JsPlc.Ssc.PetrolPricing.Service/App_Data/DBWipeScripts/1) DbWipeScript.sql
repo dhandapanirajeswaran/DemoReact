8f9cfd74ee757d94bb31569036e91bca51986ff7
praveen-kumar-1 <Parveen.kumar@sainsburys.co.uk>
2015-12-15 10:00:23 +0000

    DB Reseed method with scripts - working

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Service/App_Data/DBWipeScripts/1) DbWipeScript.sql
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Service/App_Data/DBWipeScripts/1) DbWipeScript.sql
@@ -1,33 +1,42 @@
 ﻿Delete from DailyUploadStaging;
-Truncate Table  DailyUploadStaging;
+--Truncate Table  DailyUploadStaging;
+DBCC CHECKIDENT ('DailyUploadStaging', RESEED, 0);
 
 Delete from QuarterlyUploadStaging;
-Truncate Table  QuarterlyUploadStaging;
+--Truncate Table  QuarterlyUploadStaging;
+DBCC CHECKIDENT ('QuarterlyUploadStaging', RESEED, 0);
 
 Delete from ImportProcessError;
-Truncate Table ImportProcessError;
+--Truncate Table ImportProcessError;
+DBCC CHECKIDENT ('ImportProcessError', RESEED, 0);
 
 Delete from DailyPrice;
-Truncate Table  DailyPrice;
-DBCC CHECKIDENT ('DailyPrice', RESEED, 1);
+--Truncate Table  DailyPrice;
+DBCC CHECKIDENT ('DailyPrice', RESEED, 0);
 
 Delete from SitePrice;
-Truncate Table  SitePrice;
-DBCC CHECKIDENT ('SitePrice', RESEED, 1);
+--Truncate Table  SitePrice;
+DBCC CHECKIDENT ('SitePrice', RESEED, 0);
 
 Delete from FileUpload; 
+--ALTER TABLE FileUpload NOCHECK CONSTRAINT ALL;
 --Truncate Table FileUpload;
-DBCC CHECKIDENT ('FileUpload', RESEED, 1);
+--ALTER TABLE FileUpload WITH CHECK CHECK CONSTRAINT ALL;
+DBCC CHECKIDENT ('FileUpload', RESEED, 0);
 
 Delete from SiteEmail;
 --Truncate Table  SiteEmail;
-DBCC CHECKIDENT ('SiteEmail', RESEED, 1);
+DBCC CHECKIDENT ('SiteEmail', RESEED, 0);
 
 Delete from SiteToCompetitor;
 --Truncate Table  SiteToCompetitor;
+DBCC CHECKIDENT ('SiteToCompetitor', RESEED, 0);
 
 Delete from Site;
 --Truncate Table  Site;
+DBCC CHECKIDENT ('Site', RESEED, 0);
+
+-- Following table IDs inserted by Seed, so no RESEED 0 needed
 
 Delete from FuelType;
 --Truncate Table  FuelType;
@@ -40,4 +49,3 @@ Delete from ImportProcessStatus;
 
 Delete from AppConfigSettings;
 --Truncate Table  AppConfigSettings;
-
--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Service/App_Data/DummyDataScripts/1) PricesSetup.sql
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Service/App_Data/DummyDataScripts/1) PricesSetup.sql
@@ -2,26 +2,26 @@
 --            //  1334 -> 26054, 1336
 -- 2 = Unleaded, 6 = Diesel
 -- NOTE: Divide price by 1000 to get pound value, OR by 10 to ge pence value
-Use PetrolPricingRepository
+--Use PetrolPricingRepository
 
---INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
---	VALUES(	1,	26054,	2,	0,	'2015-11-30',	1069)
---INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
---    VALUES(	1,	26054,	6,	0,	'2015-11-30',	1089)
+INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
+	VALUES(	1,	26054,	2,	0,	'2015-11-30',	1069)
+INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
+    VALUES(	1,	26054,	6,	0,	'2015-11-30',	1089)
 
---INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
---    VALUES(	1,	1336 ,	2,	0,	'2015-11-30',	1059)
---INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
---    VALUES(	1,	1336 ,	6,	0,	'2015-11-30',	1089)
+INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
+    VALUES(	1,	1336 ,	2,	0,	'2015-11-30',	1059)
+INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
+    VALUES(	1,	1336 ,	6,	0,	'2015-11-30',	1089)
 
---INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
---    VALUES(	1,	99   ,	2,	0,	'2015-11-30',	1059)
---INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
---    VALUES(	1,	99   ,	6,	0,	'2015-11-30',	1089)
+INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
+    VALUES(	1,	99   ,	2,	0,	'2015-11-30',	1059)
+INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
+    VALUES(	1,	99   ,	6,	0,	'2015-11-30',	1089)
 
---INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
---    VALUES(	1,	1751 ,	2,	0,	'2015-11-30',	1109)
---INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
---    VALUES(	1,	1751 ,	6,	0,	'2015-11-30',	1119)
+INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
+    VALUES(	1,	1751 ,	2,	0,	'2015-11-30',	1109)
+INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
+    VALUES(	1,	1751 ,	6,	0,	'2015-11-30',	1119)
 
 
