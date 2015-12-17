5608b066c935ffcea5a96292b37e36f81a2c1c1c
Praveen Kumar <Praveen.kumar@sainsburys.co.uk>
2015-12-17 21:55:18 +0000

    Update 1) PricesSetup.sql
    Dummy insert script amended

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Service/App_Data/DummyDataScripts/1) PricesSetup.sql
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Service/App_Data/DummyDataScripts/1) PricesSetup.sql
@@ -3,25 +3,27 @@
 -- 2 = Unleaded, 6 = Diesel
 -- NOTE: Divide price by 1000 to get pound value, OR by 10 to ge pence value
 --Use PetrolPricingRepository
+Declare @firstFileId int
+Set @firstFileId = (Select Top 1 Id from FileUpload Order By Id Asc)
 
 INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
-	VALUES(	1,	26054,	2,	0,	'2015-11-30',	1069)
+	Values ( @firstFileId,	26054,	2,	0,	'2015-11-30',	1069)
 INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
-    VALUES(	1,	26054,	6,	0,	'2015-11-30',	1089)
+    Values ( @firstFileId,	26054,	6,	0,	'2015-11-30',	1089)
 
 INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
-    VALUES(	1,	1336 ,	2,	0,	'2015-11-30',	1059)
+    Values ( @firstFileId,	1336 ,	2,	0,	'2015-11-30',	1059)
 INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
-    VALUES(	1,	1336 ,	6,	0,	'2015-11-30',	1089)
+    Values ( @firstFileId,	1336 ,	6,	0,	'2015-11-30',	1089)
 
 INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
-    VALUES(	1,	99   ,	2,	0,	'2015-11-30',	1059)
+    Values ( @firstFileId,	99   ,	2,	0,	'2015-11-30',	1059)
 INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
-    VALUES(	1,	99   ,	6,	0,	'2015-11-30',	1089)
+    Values ( @firstFileId,	99   ,	6,	0,	'2015-11-30',	1089)
 
 INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
-    VALUES(	1,	1751 ,	2,	0,	'2015-11-30',	1109)
+    Values ( @firstFileId,	1751 ,	2,	0,	'2015-11-30',	1109)
 INSERT INTO [dbo].[DailyPrice] ([DailyUploadId],[CatNo],[FuelTypeId],[AllStarMerchantNo],[DateOfPrice],[ModalPrice])
-    VALUES(	1,	1751 ,	6,	0,	'2015-11-30',	1119)
+    Values ( @firstFileId,	1751 ,	6,	0,	'2015-11-30',	1119)
 
 
