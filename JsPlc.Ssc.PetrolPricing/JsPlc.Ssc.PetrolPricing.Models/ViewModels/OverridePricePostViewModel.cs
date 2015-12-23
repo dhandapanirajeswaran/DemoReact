ff84959d5683199305ccf8ac194ab882d785051d
praveen-kumar-1 <Parveen.kumar@sainsburys.co.uk>
2015-12-23 16:46:27 +0000

    Override Prices viewModel for saving prices

---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Models/ViewModels/OverridePricePostViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsPlc.Ssc.PetrolPricing.Models.ViewModels
+{
+    // Used to Postback data from Pricing page
+    public class OverridePricePostViewModel
+    {
+        [Required]
+        public string SiteId { get; set; } // JSSiteId
+        [Required]
+        public string FuelTypeId { get; set; } // 1,2,6 values
+
+        [Range(1d, 400d, ErrorMessage = "Override price should be between 1 and 400")] // prevent user postback of prices over £4
+        public float OverridePrice { get; set; } // from user input 
+    }
+}
