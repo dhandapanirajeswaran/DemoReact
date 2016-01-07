48bf58ee054a6918f8cf81cfe06fbcefa53e92da
ilyas-dhin <ilyas.dhin@sainsburys.co.uk>
2016-01-07 09:51:21 +0000

    Added price point report object

---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Models/ViewModels/PricePointReportItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace JsPlc.Ssc.PetrolPricing.Models.ViewModels
+{
+    public class PricePointReportItem
+    {
+        public decimal Price { get; set; }
+
+        public List<PricePointBrandCount> PricePointBrands { get; set; }
+
+        public PricePointReportItem()
+        {
+            PricePointBrands = new List<PricePointBrandCount>();
+        }
+    }
+}
---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Models/ViewModels/PricePointReportViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace JsPlc.Ssc.PetrolPricing.Models.ViewModels
+{
+    public class PricePointReportViewModel
+    {
+        public string FuelTypeName { get; set; }
+
+        public List<PricePointReportItem> PricePointReportItems { get; set; }
+
+        public PricePointReportViewModel()
+        {
+            PricePointReportItems = new List<PricePointReportItem>();
+        }
+    }
+}
