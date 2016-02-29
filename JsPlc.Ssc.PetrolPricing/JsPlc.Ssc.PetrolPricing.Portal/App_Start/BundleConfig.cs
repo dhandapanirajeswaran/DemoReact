9ff7ae379f4504a82a5e4d640c2e02f469c87176
andreyshihovsainsburys <andrey.shihov@sainsburys.co.uk>
2016-02-29 16:20:05 +0000

    FC-57 / FC:'Show all' button alignment need to be changed in petrol pricing application screen FC-58 / FC:Search by town attribute is not working in sainsburys site screen FC-59 / FC:User is redirected to home screen upon click on 'Account' button in petrol pricing application FC-54 / Tidy up site look and feel.

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/App_Start/BundleConfig.cs
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/App_Start/BundleConfig.cs
@@ -11,8 +11,10 @@ namespace JsPlc.Ssc.PetrolPricing.Portal
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            //Unobtrusive validation doesn't work. Temporary disabling
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        //"~/Scripts/jquery.validate*")
+                        ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -21,8 +23,7 @@ namespace JsPlc.Ssc.PetrolPricing.Portal
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       //"~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/site.js"));
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
