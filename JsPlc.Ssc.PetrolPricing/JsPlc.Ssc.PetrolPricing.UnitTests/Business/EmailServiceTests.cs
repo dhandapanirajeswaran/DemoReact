8ae2e87f3b42678a1eadae25b85e148680a1c7d7
andreyshihovsainsburys <andrey.shihov@sainsburys.co.uk>
2016-03-08 14:06:29 +0000

    FC-73 / Build unit test for BuildEmailBody method - ensure that body is not empty (means there is price change)

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.UnitTests/Business/EmailServiceTests.cs
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.UnitTests/Business/EmailServiceTests.cs
@@ -47,12 +47,14 @@ namespace JsPlc.Ssc.PetrolPricing.UnitTests.Business
 		}
 
 		[TestCase(BuildEmailBodyTestCases.NoPreviousTradeDatePriceFound)]
-		public void When_BuildEmailBody_MethodCalled_And_There_Is_No_Prices_For_The_Previous_Trade_Day_Then_Email_Body_Should_Be_Returned
+		[TestCase(BuildEmailBodyTestCases.FuelTypeTradeChange)]
+		[TestCase(BuildEmailBodyTestCases.PriceDifferenceChange)]
+		public void When_BuildEmailBody_MethodCalled_And_All_Requirements_Met_Then_Valid_Email_Body_Should_Be_Returned
 		(
 			BuildEmailBodyTestCases testCase
 		)
 		{
-			
+
 			//Arrange
 			#region Arrange
 			if (testCase == BuildEmailBodyTestCases.NoPreviousTradeDatePriceFound)
@@ -66,15 +68,46 @@ namespace JsPlc.Ssc.PetrolPricing.UnitTests.Business
 						OverriddenPrice = 1001
 					}
 				};
-			} else if (testCase == BuildEmailBodyTestCases.FuelTypeTradeChange)
+			}
+			else if (testCase == BuildEmailBodyTestCases.FuelTypeTradeChange)
 			{
-				//test when there is no price for the previous trade day
+				//test when fuels has been added/removed
+				_site.Prices = new List<Models.SitePrice> { 
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today,
+						FuelTypeId = 2,
+						SuggestedPrice = 1004,
+						OverriddenPrice = 1005
+					},
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today,
+						FuelTypeId = 1,
+						SuggestedPrice = 1000,
+						OverriddenPrice = 1001
+					},
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today.AddDays(-1),
+						FuelTypeId = 1,
+						SuggestedPrice = 1002,
+						OverriddenPrice = 1003
+					}
+				};
+			}
+			else if (testCase == BuildEmailBodyTestCases.PriceDifferenceChange)
+			{
+				//test when fuel price has changed
 				_site.Prices = new List<Models.SitePrice> { 
 					new Models.SitePrice {
 						DateOfCalc = DateTime.Today,
 						FuelTypeId = 1,
 						SuggestedPrice = 1000,
 						OverriddenPrice = 1001
+					},
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today.AddDays(-1),
+						FuelTypeId = 1,
+						SuggestedPrice = 1002,
+						OverriddenPrice = 1003
 					}
 				};
 			}
@@ -89,6 +122,92 @@ namespace JsPlc.Ssc.PetrolPricing.UnitTests.Business
 
 			//overriden price appeard in email's body
 			Assert.IsTrue(result.Contains("100.1"));
+		}
+
+		[TestCase(BuildEmailBodyTestCases.NoPreviousTradeDatePriceFound)]
+		[TestCase(BuildEmailBodyTestCases.FuelTypeTradeChange)]
+		[TestCase(BuildEmailBodyTestCases.PriceDifferenceChange)]
+		public void When_BuildEmailBody_MethodCalled_And_Some_Requirements_Are_NOT_Met_Then_EMPTY_Email_Body_Should_Be_Returned
+		(
+			BuildEmailBodyTestCases testCase
+		)
+		{
+			//Arrange
+			#region Arrange
+			if (testCase == BuildEmailBodyTestCases.NoPreviousTradeDatePriceFound)
+			{
+				//test when there is price for the previous trade day and it's not changed
+				_site.Prices = new List<Models.SitePrice> { 
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today,
+						FuelTypeId = 1,
+						SuggestedPrice = 1000,
+						OverriddenPrice = 1001
+					}, 
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today.AddDays(-1),
+						FuelTypeId = 1,
+						SuggestedPrice = 1000,
+						OverriddenPrice = 1001
+					}
+				};
+			}
+			else if (testCase == BuildEmailBodyTestCases.FuelTypeTradeChange)
+			{
+				//test when there is no price for the previous trade day
+				_site.Prices = new List<Models.SitePrice> { 
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today,
+						FuelTypeId = 2,
+						SuggestedPrice = 1004,
+						OverriddenPrice = 1005
+					},
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today.AddDays(-1),
+						FuelTypeId = 2,
+						SuggestedPrice = 1004,
+						OverriddenPrice = 1005
+					},
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today,
+						FuelTypeId = 1,
+						SuggestedPrice = 1002,
+						OverriddenPrice = 1003
+					},
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today.AddDays(-1),
+						FuelTypeId = 1,
+						SuggestedPrice = 1002,
+						OverriddenPrice = 1003
+					}
+				};
+			}
+			else if (testCase == BuildEmailBodyTestCases.PriceDifferenceChange)
+			{
+				//test when there is no price for the previous trade day
+				_site.Prices = new List<Models.SitePrice> { 
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today,
+						FuelTypeId = 1,
+						SuggestedPrice = 1002,
+						OverriddenPrice = 1003
+					},
+					new Models.SitePrice {
+						DateOfCalc = DateTime.Today.AddDays(-1),
+						FuelTypeId = 1,
+						SuggestedPrice = 1002,
+						OverriddenPrice = 1003
+					}
+				};
+			}
+			#endregion
+
+			//Act
+			var result = EmailService.BuildEmailBody(_site, DateTime.Today);
+
+			//Assert
+			//email body is not empty
+			Assert.IsEmpty(result);
 		}
 	}
 }
