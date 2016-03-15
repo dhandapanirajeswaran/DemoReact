fee772f3350d6b9b7b237a9d8d91bdd769703fbf
gopisaba <tlk2gopisaba@gmail.com>
2016-03-15 14:37:24 +0000

    Removed  variable for windir

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Service/deploy.ps1
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Service/deploy.ps1
@@ -1,1 +1,1 @@
-$env:windir\System32\inetsrv\appcmd.exe set apppool /apppool.name:petrolpricing_service_pool /enable32BitAppOnWin64:true
+c:\Windows\System32\inetsrv\appcmd.exe set apppool /apppool.name:petrolpricing_service_pool /enable32BitAppOnWin64:true
