c4ce8642971e58ac5d0fb1b71fa9f176c7ebd236
gopisaba <tlk2gopisaba@gmail.com>
2016-03-15 14:51:03 +0000

    Fixed the apppool name for potal in deploy.ps1

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/deploy.ps1
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/deploy.ps1
@@ -1,1 +1,1 @@
-c:\Windows\System32\inetsrv\appcmd.exe set apppool /apppool.name:petrolpricing_service_pool /enable32BitAppOnWin64:true
+c:\Windows\System32\inetsrv\appcmd.exe set apppool /apppool.name:petrolpricing_portal_pool /enable32BitAppOnWin64:true
