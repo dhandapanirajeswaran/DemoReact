2f80c53c3e5355394d90eda7462e45b078250eab
ramarajuvittanala <ramaraju.vittanala@sainsburys.co.uk>
2016-05-11 16:38:07 +0100

    LogOff Issue

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Controllers/AccountController.cs
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.OpenIdConnect;
 using Microsoft.Owin.Security.Cookies;
+using System.Web.Security;
 
 namespace JsPlc.Ssc.PetrolPricing.Portal.Controllers
 {
@@ -20,6 +22,18 @@ namespace JsPlc.Ssc.PetrolPricing.Portal.Controllers
         [HttpGet]
         public ActionResult LogOff()
         {
+
+            HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                authCookie.Expires = DateTime.Now.AddDays(-1d);
+                Request.Cookies.Remove(FormsAuthentication.FormsCookieName);
+                var timeout = int.Parse(ConfigurationManager.AppSettings["SessionTimeout"]);
+                HttpCookie faCookie = new HttpCookie(FormsAuthentication.FormsCookiePath, "");
+                faCookie.Expires = DateTime.Now.AddMinutes(timeout);
+                HttpContext.Response.Cookies.Add(faCookie);
+            }
+       
             AuthenticationManager.SignOut(OpenIdConnectAuthenticationDefaults.AuthenticationType, 
                 CookieAuthenticationDefaults.AuthenticationType);
             return RedirectToAction("Index", "Home");
