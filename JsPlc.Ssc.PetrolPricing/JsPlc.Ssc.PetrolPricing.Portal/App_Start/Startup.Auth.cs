ac98b0a724eda042a61a9457ef76d7510b422bdb
ramarajuvittanala <ramaraju.vittanala@sainsburys.co.uk>
2016-05-19 14:34:37 +0100

    Revert previous changes

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/App_Start/Startup.Auth.cs
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/App_Start/Startup.Auth.cs
@@ -20,7 +20,7 @@ using System.Web.Security;
 
 namespace JsPlc.Ssc.PetrolPricing.Portal
 {
-   
+
 
     public partial class Startup
     {
@@ -34,65 +34,65 @@ namespace JsPlc.Ssc.PetrolPricing.Portal
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
-			app.UseKentorOwinCookieSaver();
+            app.UseKentorOwinCookieSaver();
 
             var timeout = int.Parse(ConfigurationManager.AppSettings["SessionTimeout"]);
 
-			app.UseCookieAuthentication(
-				new CookieAuthenticationOptions
-				{
-					AuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
-					CookieSecure = CookieSecureOption.SameAsRequest,
-                    ExpireTimeSpan = new TimeSpan(0,timeout,0),
-                    SlidingExpiration= false,
-					Provider = new CookieAuthenticationProvider
-					{
-                      
-                    	OnResponseSignedIn = (context) =>
-						{
-							if (context.Identity.IsAuthenticated)
-							{
-								var facade = new ServiceFacade();
-								var array = context.Identity.Name.Split(new[] { '#' });
-								var userName = string.Empty;
-								if (array == null || array.Length == 0)
-								{
-									userName = context.Identity.Name;
-								}
-								else
-								{
-									if (array.Length == 1)
-									{
-										userName = context.Identity.Name.Split(new[] { '#' })[0];
-									}
-									else
-									{
-										userName = context.Identity.Name.Split(new[] { '#' })[1];
-									}
-								}
-								facade.RegisterUser(userName);
-							}
-						}
-					}
-				});
-
-			app.UseOpenIdConnectAuthentication(
-				new OpenIdConnectAuthenticationOptions
-				{
-					SignInAsAuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType,
-					ClientId = clientId,
-					Authority = Authority,
-					PostLogoutRedirectUri = postLogoutRedirectUri,
-					RedirectUri = postLogoutRedirectUri,   
-                    UseTokenLifetime=false,
-                    BackchannelTimeout = new TimeSpan(0, timeout, 0),
-                    RefreshOnIssuerKeyNotFound=false,
-
-                    Notifications= new OpenIdConnectAuthenticationNotifications()
+            app.UseCookieAuthentication(
+                new CookieAuthenticationOptions
+                {
+                    AuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
+                    CookieSecure = CookieSecureOption.SameAsRequest,
+                    ExpireTimeSpan = new TimeSpan(0, timeout, 0),
+                    SlidingExpiration = false,
+                    Provider = new CookieAuthenticationProvider
                     {
-                         RedirectToIdentityProvider = (context) =>
+
+                        OnResponseSignedIn = (context) =>
+                        {
+                            if (context.Identity.IsAuthenticated)
+                            {
+                                var facade = new ServiceFacade();
+                                var array = context.Identity.Name.Split(new[] { '#' });
+                                var userName = string.Empty;
+                                if (array == null || array.Length == 0)
+                                {
+                                    userName = context.Identity.Name;
+                                }
+                                else
+                                {
+                                    if (array.Length == 1)
+                                    {
+                                        userName = context.Identity.Name.Split(new[] { '#' })[0];
+                                    }
+                                    else
+                                    {
+                                        userName = context.Identity.Name.Split(new[] { '#' })[1];
+                                    }
+                                }
+                                facade.RegisterUser(userName);
+                            }
+                        }
+                    }
+                });
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    SignInAsAuthenticationType = OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                    ClientId = clientId,
+                    Authority = Authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    RedirectUri = postLogoutRedirectUri,
+                    UseTokenLifetime = false,
+                    BackchannelTimeout = new TimeSpan(0, timeout, 0),
+                    RefreshOnIssuerKeyNotFound = false,
+
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                        RedirectToIdentityProvider = (context) =>
                         {
                             loginTimeTicks = DateTime.Now.Ticks;
                             var user = context.OwinContext.Authentication.User;
@@ -100,49 +100,49 @@ namespace JsPlc.Ssc.PetrolPricing.Portal
                             return Task.FromResult(0);
 
                         },
-                        MessageReceived =(context)=>
-                            {
-                                long MessageReceivedTimeTicks = DateTime.Now.Ticks;
-                                long inSec = (MessageReceivedTimeTicks - loginTimeTicks) / (60 * 60 * 60);
-                               
-                                if (inSec > 50) //when user enter input
-                                {
-                                    UiHelper.CreateAuthCookie1();
-                                    UiHelper.CreateAuthCookie2();                                   
-                                }
-                                UiHelper.bIsFirstStartupAuthCalled = true;
-                                return Task.FromResult(0);
-                            },
-                         AuthorizationCodeReceived= (context) =>
-                            {
-                                var code = context.Code;
-                                JwtSecurityToken tokenold = context.JwtSecurityToken;
+                        MessageReceived = (context) =>
+                        {
+                            long MessageReceivedTimeTicks = DateTime.Now.Ticks;
+                            long inSec = (MessageReceivedTimeTicks - loginTimeTicks) / (60 * 60 * 60);
 
-                                context.AuthenticationTicket.Properties.IssuedUtc = DateTime.Now;
-                                context.AuthenticationTicket.Properties.ExpiresUtc = DateTime.Now.AddMinutes(timeout);
-                           
-                               var lst = tokenold.Audiences;
-                            context.JwtSecurityToken = new JwtSecurityToken(tokenold.Issuer,
-                               lst.FirstOrDefault(), 
-                               tokenold.Claims, 
-                               DateTime.Now,
-                               DateTime.Now.AddMinutes(timeout), 
-                               tokenold .SigningCredentials);
-                                return Task.FromResult(0);
+                            if (inSec > 50) //when user enter input
+                            {
+                                UiHelper.CreateAuthCookie1();
+                                UiHelper.CreateAuthCookie2();
                             }
+                            UiHelper.bIsFirstStartupAuthCalled = true;
+                            return Task.FromResult(0);
+                        },
+                        AuthorizationCodeReceived = (context) =>
+                        {
+                            var code = context.Code;
+                            JwtSecurityToken tokenold = context.JwtSecurityToken;
+
+                            context.AuthenticationTicket.Properties.IssuedUtc = DateTime.Now;
+                            context.AuthenticationTicket.Properties.ExpiresUtc = DateTime.Now.AddMinutes(timeout);
+
+                            var lst = tokenold.Audiences;
+                            context.JwtSecurityToken = new JwtSecurityToken(tokenold.Issuer,
+                               lst.FirstOrDefault(),
+                               tokenold.Claims,
+                               DateTime.Now,
+                               DateTime.Now.AddMinutes(timeout),
+                               tokenold.SigningCredentials);
+                            return Task.FromResult(0);
+                        }
                     },
                     TokenValidationParameters = new System.IdentityModel.Tokens.TokenValidationParameters
                     {
                         ValidateIssuer = false,
-                        RequireExpirationTime=true,
-                        ValidateLifetime=false
+                        RequireExpirationTime = true,
+                        ValidateLifetime = false
                     }
 
-				});
+                });
 
-            
 
-			AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
         }
     }
 }
