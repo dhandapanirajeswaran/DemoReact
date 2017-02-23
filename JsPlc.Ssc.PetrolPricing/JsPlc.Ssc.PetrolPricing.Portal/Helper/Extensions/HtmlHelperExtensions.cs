7c6697996573323d3a179911d632fd7d5fcfe367
garry-leeder <garry.leeder@sainsburys.co.uk>
2017-02-23 12:27:07 +0000

    Tweaks to file upload (busy loader, back button, friendly date formatting)

---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Helper/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JsPlc.Ssc.PetrolPricing.Portal.Helper.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string FormatFriendlyDateTime(this HtmlHelper helper,DateTime datetime)
+        {
+            var daysAgo = DateTime.Now.Date.Subtract(datetime.Date).TotalDays;
+            var formattedTime = datetime.ToString("HH:mm:ss");
+            if (daysAgo == 0)
+                return "Today at " + formattedTime;
+            if (daysAgo == 1)
+                return "Yesterday at " + formattedTime;
+            return datetime.ToString("dd-MMM-yyyy HH:mm:ss");
+        }
+    }
+}
