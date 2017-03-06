cec6c6deabae8ef2515c175d70841c655eee5c98
garry-leeder <garry.leeder@sainsburys.co.uk>
2017-03-06 10:25:47 +0000

    Refactored day columns JS. Tweaks to override input.

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Scripts/App/prices.js
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Scripts/App/prices.js
@@ -44,66 +44,133 @@ function getRootSiteFolder() {
     return rootFolder;
 }
 
-function refreshDates(selectedDate) {
-    selectedDate = selectedDate == "" ? $('#viewingDate').val() : selectedDate;
-    var yesterday = new Date();
-    var tomorrow = new Date();
-    var strtoday = "";
-    var stryday = "";
-    var strtomorrow = "";
-    if (selectedDate == "") {
-        var d = new Date();
+function padZero2Digit(n) {
+    return (n < 10 ? '0' : '') + n;
+}
 
-        var month = d.getMonth() + 1;
-        var day = d.getDate();
-        var year = d.getFullYear();
-        d = new Date(yearnum, monnum, daynum - 1);
-        strtoday = (('' + day).length < 2 ? '0' : '') + day + '/' +
-            (('' + month).length < 2 ? '0' : '') + month + '/' +
-            d.getFullYear();
+function dateAdd(unit, delta, datetime) {
+    var day = datetime.getDate(),
+        month = datetime.getMonth() + 1,
+        year = datetime.getFullYear();
 
-        yesterday = new Date(d.getFullYear(), d.getMonth(), d.getDate() - 1);
-        tomorrow = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1);
-    } else {
-        strtoday = selectedDate;
-        var tmp = selectedDate.split("/");
-        var yearnum = parseInt(tmp[2]);
-        var monnum = parseInt(tmp[1]) - 1;
-        var daynum = parseInt(tmp[0]);
-
-        var d = new Date(yearnum, monnum, daynum - 1);
-        yesterday = new Date(d.getFullYear(), d.getMonth(), d.getDate() - 1);
-        tomorrow = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 2);
-
-        var month1 = d.getMonth() + 1;
-        var day1 = d.getDate();
-        var year1 = d.getFullYear();
-        strtoday = (('' + day1).length < 2 ? '0' : '') + day1 + '/' +
-            (('' + month1).length < 2 ? '0' : '') + month1 + '/' +
-            d.getFullYear();
+    switch(unit.toLowerCase()) {
+        case 'd':
+        case 'dd':
+        case 'day':
+            day+=delta;
+            break;
+        case 'm':
+        case 'mm':
+        case 'month':
+            month+=delta;
+            break;
+        case 'y':
+        case 'yy':
+        case 'year':
+            year+=delta;
+            break;
+        default:
+            throw new Error('dateAdd - unsupported unit type: ' + unit);
+            break;
     }
 
-    var month = yesterday.getMonth() + 1;
-    var day = yesterday.getDate();
-    stryday = (('' + day).length < 2 ? '0' : '') + day + '/' +
-        (('' + month).length < 2 ? '0' : '') + month + '/' +
-        yesterday.getFullYear();
+    return new Date(year, month - 1, day);
+}
 
-    month = tomorrow.getMonth() + 1;
-    day = tomorrow.getDate();
-    strtomorrow = (('' + day).length < 2 ? '0' : '') + day + '/' +
-        (('' + month).length < 2 ? '0' : '') + month + '/' +
-        tomorrow.getFullYear();
-    $('#today1').text(strtoday);
-    $('#today2').text(strtoday);
-    $('#today3').text(strtoday);
+function isUKDate(text) {
+    return /\d{1,2}\/\d{1,2}\/\d{4}/.test(text);
+}
 
-    $(".comptoday").text(strtoday);
+function isSameDate(datetime1, datetime2) {
+    return datetime1.getFullYear() == datetime2.getFullYear()
+        && datetime1.getMonth() == datetime2.getMonth()
+        && datetime1.getDate() == datetime2.getDate();
+}
 
-    $('#tomorrow1').text(strtomorrow);
-    $('#tomorrow2').text(strtomorrow);
-    $('#tomorrow3').text(strtomorrow);
-    $(".compyday").text(stryday);
+///
+/// formatUKDate(date)
+/// formatUKDate(year, month, day)
+///
+function formatUKDate() {
+    var year, month, day, datetime;
+
+    if (arguments.length == 1 && arguments[0] instanceof Date) {
+        datetime = arguments[0];
+    } else if (arguments.length == 3) {
+        day = parseInt(arguments[0], 10);
+        month = parseInt(arguments[1], 10);
+        year = parseInt(arguments[2], 10);
+        datetime = new Date(year, month - 1, day);
+    }
+
+    if (datetime == undefined) 
+        throw new Error('formatUKDate - unsupported argument signature');
+
+    day = datetime.getDate(),
+    month = datetime.getMonth() + 1;
+    year = datetime.getFullYear();
+
+    return padZero2Digit(day) + '/' + padZero2Digit(month) + '/' + year;
+};
+
+function buildRelativeDayHtml(datetime) {
+    var today = new Date(),
+        timeDiff = datetime.getTime() - today.getTime(),
+        diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+        switch (diffDays) {
+            case -1:
+                return '<b class="yesterday-date">Yesterday</b>';
+            case 0:
+                return '<b class="today-date">Today</b>';
+            case 1:
+                return '<b class="tomorrow-date">Tomorrow</b>';
+            default:
+                if (diffDays < 0)
+                    return '<b class="past-date">' + diffDays + ' Days</b>';
+                else
+                    return '<b class="future-date">' + diffDays + ' Days</b>';
+        }
+};
+
+function buildDateHeadingAndMarkup(datetime) {
+    return {
+        datetime: new Date(datetime),
+        formatted: formatUKDate(datetime),
+        markup: buildRelativeDayHtml(datetime)
+    };
+}
+
+function refreshDates(selectedDate) {
+    selectedDate = selectedDate == "" ? $('#viewingDate').val() : selectedDate;
+
+    var currentDate,
+        previousDay,
+        currentDay,
+        nextDay;
+
+    if (isUKDate(selectedDate)) {
+        dateParts = selectedDate.split('/');
+        currentDate = new Date(parseInt(dateParts[2], 10), parseInt(dateParts[1], 10) - 1, parseInt(dateParts[0], 10));
+    } else {
+        currentDate = new Date();
+    }
+
+    currentDay = buildDateHeadingAndMarkup(currentDate);
+    previousDay = buildDateHeadingAndMarkup(dateAdd('d', -1, currentDate));
+    nextDay = buildDateHeadingAndMarkup(dateAdd('d', +1, currentDate));
+
+    $('#today1').html(previousDay.markup + '<br />' + previousDay.formatted );
+    $('#today2').html(previousDay.markup + '<br />' + previousDay.formatted);
+    $('#today3').html(previousDay.markup + '<br />' + previousDay.formatted);
+
+    $(".comptoday").html(currentDay.formatted);
+
+    $('#tomorrow1').html(nextDay.markup + '<br />' + nextDay.formatted);
+    $('#tomorrow2').html(nextDay.markup + '<br />' + nextDay.formatted);
+    $('#tomorrow3').html(nextDay.markup + '<br />' + nextDay.formatted);
+
+    $(".compyday").html(previousDay.formatted);
 }
 
 $("#viewingStoreNo").change(function () {
