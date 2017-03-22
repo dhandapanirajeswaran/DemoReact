7b92dfa77e43dfdaf78fc5b5490c1d9f07110265
garry-leeder <garry.leeder@sainsburys.co.uk>
2017-03-22 14:55:15 +0000

    Refactored top nav highlight JS

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Scripts/App/layout.js
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Portal/Scripts/App/layout.js
@@ -1,83 +1,39 @@
-﻿define(["bootstrap", "scrollToTop"],
-    function (bootstrap, scrollToTop) {
+﻿define(["jquery", "bootstrap", "scrollToTop"],
+    function ($, bootstrap, scrollToTop) {
         "use strict";
 
+        var navSectionMap = {
+            "nav-home": ["^\/$"],
+            "nav-login": ["^/account/login"],
+            "nav-account": ["^/account", "^/manage"],
+            "nav-about": ["^/home/about"],
+            "nav-contact": ["^/home/contact"],
+            "nav-upload": ["^/file"],
+            "nav-site-pricing": ["^/sites/prices"],
+            "nav-site-maintenance": ["^/sites"],
+            "nav-reports": ["^/pricereports"],
+            "nav-users": ["^/ppusers"]
+        };
+
         function setNavigationTab() {
-            //navigation
+            var activeTab = null,
+                locationPath = location.pathname.toLowerCase().replace(/^\/petrolpricing\//i, '/'),
+                key,
+                paths,
+                path;
+
+            for (key in navSectionMap) {
+                paths = navSectionMap[key];
+                $.each(navSectionMap[key], function (i, value) {
+                    if (new RegExp(value, 'i').test(locationPath))
+                        activeTab = key;
+                });
+                if (activeTab)
+                    break;
+            }
             $(".navbar-nav li").removeClass("active");
-            var navItemAttributeName = "data-menu-item";
-            $(".navbar-nav li").each(function () {
-                var $this = $(this);
-                var locationPath = location.pathname.toLowerCase();
-
-                // ignore dev URL
-                locationPath = locationPath.replace(/^\/petrolpricing\//i, '/');
-                //nav-home // /
-                if (locationPath == '/'
-                    && $this.attr(navItemAttributeName) == "nav-home") {
-
-                    $this.addClass("active");
-                    return false;
-                }
-                    //nav-login // /Account/Login
-                else if (locationPath.indexOf("/account/login") == 0
-                    && $this.attr(navItemAttributeName) == "nav-login") {
-                    $this.addClass("active");
-                    return false;
-                }
-                    //nav-account // /Manage || /Account
-                else if ((locationPath.indexOf("/account") == 0
-                            || location.pathname.indexOf("/Manage") == 0
-                    )
-                    && $this.attr(navItemAttributeName) == "nav-account") {
-                    $this.addClass("active");
-                    return false;
-                }
-                    //nav-about // /Home/About
-                else if (locationPath.indexOf("/home/about") == 0
-                    && $this.attr(navItemAttributeName) == "nav-about") {
-                    $this.addClass("active");
-                    return false;
-                }
-                    //nav-contact // /Home/Contact
-                else if (locationPath.indexOf("/home/contact") == 0
-                    && $this.attr(navItemAttributeName) == "nav-contact") {
-                    $this.addClass("active");
-                    return false;
-                }
-                    //nav-upload // /File
-                else if (locationPath.indexOf("/file") == 0
-                    && $this.attr(navItemAttributeName) == "nav-upload") {
-                    $this.addClass("active");
-                    return false;
-                }
-                    //nav-site-pricing // /Sites/Prices
-                else if (locationPath.indexOf("/sites/prices") == 0
-                    && $this.attr(navItemAttributeName) == "nav-site-pricing") {
-                    $this.addClass("active");
-                    return false;
-                }
-                    //nav-site-maintenance // /Sites
-                else if (locationPath.indexOf("/sites") == 0
-                    && $this.attr(navItemAttributeName) == "nav-site-maintenance") {
-                    $this.addClass("active");
-                    return false;
-                }
-                    //nav-reports // /PriceReports
-                else if (locationPath.indexOf("/pricereports") == 0
-                    && $this.attr(navItemAttributeName) == "nav-reports") {
-                    $this.addClass("active");
-                    return false;
-                }
-
-                    //nav-reports // /Users
-                else if (locationPath.indexOf("/ppusers") == 0
-                    && $this.attr(navItemAttributeName) == "nav-users") {
-                    $this.addClass("active");
-                    return false;
-                }
-            });
-        }
+            $(".navbar-nav li[data-menu-item='" + activeTab + "']").addClass("active");
+        };
 
         function init() {
             scrollToTop.init();
