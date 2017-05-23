781f21ffdcf88cd9ccc9c7c33b2607ce9f621aa6
garry-leeder <garry.leeder@sainsburys.co.uk>
2017-05-23 17:13:01 +0100

    FC-209 - Price Change Variance (updated SP to bring back rounded AutoPrice)

---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Databases/PetrolPricingRepository/dbo/Functions/Scalar Functions/fn_RoundTomorrowPriceByPriceChangeVariance.sql
@@ -0,0 +1,20 @@
+﻿CREATE FUNCTION [dbo].[fn_RoundTomorrowPriceByPriceChangeVariance]
+(
+	@PriceChangeVarianceThreshold INT,
+	@TodayPrice INT,
+	@TomorrowPrice INT
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @RoundedTomorrowPrice INT = @TomorrowPrice
+
+	IF @TodayPrice > 0 AND @TomorrowPrice > 0
+	BEGIN
+		DECLARE @Diff INT = ABS(@TomorrowPrice - @TodayPrice)
+		IF @Diff <= @PriceChangeVarianceThreshold
+			SET @RoundedTomorrowPrice = @TodayPrice
+	END
+
+	RETURN @RoundedTomorrowPrice
+END
