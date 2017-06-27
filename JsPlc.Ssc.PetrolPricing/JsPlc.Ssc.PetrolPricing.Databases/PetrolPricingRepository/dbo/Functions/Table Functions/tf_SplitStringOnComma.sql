51c3fd59d88fe65ce2affe0f612f83be654936ea
garry-leeder <garry.leeder@sainsburys.co.uk>
2017-06-27 17:05:23 +0100

    settings - Excluded Brands fix for empty string.

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Databases/PetrolPricingRepository/dbo/Functions/Table Functions/tf_SplitStringOnComma.sql
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Databases/PetrolPricingRepository/dbo/Functions/Table Functions/tf_SplitStringOnComma.sql
@@ -9,14 +9,17 @@ BEGIN
 	DECLARE @StartIndex INT = 0
 	DECLARE @EndIndex INT = CHARINDEX(',', @CommaString)
 
-	WHILE @EndIndex > 0
+	IF LEN(@CommaString) > 0 
 	BEGIN
-		INSERT INTO @Slices 
-		VALUES (SUBSTRING(@CommaString, @StartIndex, @EndIndex - @StartIndex))
-		SET @StartIndex = @EndIndex + 1
-		SET @EndIndex = CHARINDEX(',', @CommaString, @StartIndex+1)
+		WHILE @EndIndex > 0
+		BEGIN
+			INSERT INTO @Slices 
+			VALUES (SUBSTRING(@CommaString, @StartIndex, @EndIndex - @StartIndex))
+			SET @StartIndex = @EndIndex + 1
+			SET @EndIndex = CHARINDEX(',', @CommaString, @StartIndex+1)
+		END
+		INSERT INTO @Slices
+		VALUES(SUBSTRING(@CommaString, @StartIndex, 1 + LEN(@CommaString) - @StartIndex))
 	END
-	INSERT INTO @Slices
-	VALUES(SUBSTRING(@CommaString, @StartIndex, 1 + LEN(@CommaString) - @StartIndex))
 	RETURN 
 END
