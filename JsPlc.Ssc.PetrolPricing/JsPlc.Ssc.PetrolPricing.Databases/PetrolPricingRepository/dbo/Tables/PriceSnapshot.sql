dd066a46ef7266122b44d960b7f90afda3f76fda
garry-leeder <garry.leeder@sainsburys.co.uk>
2017-07-03 15:37:44 +0100

    FC-239 added PriceSnapshot tables

---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Databases/PetrolPricingRepository/dbo/Tables/PriceSnapshot.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[PriceSnapshot]
+(
+	[PriceSnapshotId] [int] NOT NULL IDENTITY,
+	[DateFrom] [date] NOT NULL,
+	[DateTo] [date] NOT NULL,
+	[CreatedOn] [datetime2](7) NOT NULL,
+	[UpdatedOn] [datetime2](7) NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[IsOutdated] [bit] NOT NULL, 
+    CONSTRAINT [PK_PriceSnapshot] PRIMARY KEY ([PriceSnapshotId]),
+)
---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Databases/PetrolPricingRepository/dbo/Tables/PriceSnapshotRow.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[PriceSnapshotRow]
+(
+	[PriceSnapshotId] [int] NOT NULL,
+	[SiteId] [int] NULL,
+	[FuelTypeId] [int] NULL,
+	[AutoPrice] [int] NULL,
+	[OverridePrice] [int] NULL,
+	[TodayPrice] [int] NULL,
+	[Markup] [float] NULL,
+	[CompetitorName] [nvarchar](max) NULL,
+	[IsTrailPrice] [int] NULL,
+	[CompetitorPriceOffset] [float] NULL,
+	[PriceMatchType] [int] NULL,
+	[PriceSource] [varchar](19) NULL,
+	[PriceSourceDateTime] [datetime] NULL,
+	[CompetitorSiteId] [int] NULL,
+	[Distance] [real] NULL,
+	[DriveTime] [real] NULL,
+	[DriveTimePence] [int] NULL 
+)
