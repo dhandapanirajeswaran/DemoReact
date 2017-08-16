1051e6679a6c46a5f3e1123b2a8bc527dc62a410
garry-leeder <garry.leeder@sainsburys.co.uk>
2017-08-16 15:50:49 +0100

    pricing - historical prices removed gaps in dates/fuel combos

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Databases/PetrolPricingRepository/dbo/Stored Procedures/spGetHistoricalPricesForSite.sql
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Databases/PetrolPricingRepository/dbo/Stored Procedures/spGetHistoricalPricesForSite.sql
@@ -5,23 +5,53 @@
 AS
 	SET NOCOUNT ON;
 
+----DEBUG:START
+--DECLARE	@SiteId INT = 6164
+--DECLARE	@StartDate DATE = DATEADD(DAY, -30, GETDATE())
+--DECLARE @EndDate DATE = GETDATE()
+----DEBUG:END
+
 	;WITH DateRange As  
 	(  
 		SELECT @StartDate AS TheDate
 		UNION ALL  
 		SELECT DATEADD(DAY,1, TheDate) FROM DateRange
 		WHERE TheDate < @EndDate  
+	),
+	FuelTypes AS (
+		SELECT 1 [FuelTypeId]
+		UNION ALL
+		SELECT 2 [FuelTypeId]
+		UNION ALL
+		SELECT 6 [FuelTypeId]
+	),
+	HistoricalPrices AS (
+		SELECT
+			dr.TheDate [PriceDate],
+			@SiteId [SiteId],
+			hfp.FuelTypeId [FuelTypeId],
+			hfp.TodayPrice [TodayPrice],
+			PriceSource [PriceSource]
+		FROM
+			DateRange dr
+			CROSS APPLY dbo.tf_GetHistoricalSiteFuelPricesForDay(dr.TheDate, @SiteId) hfp
 	)
 	SELECT
 		dr.TheDate [PriceDate],
 		@SiteId [SiteId],
-		hfp.FuelTypeId [FuelTypeId],
-		COALESCE(hfp.TodayPrice, 0) [TodayPrice],
-		COALESCE(hfp.PriceSource, '') [PriceSource]
+		ft.FuelTypeId [FuelTypeId],
+		COALESCE(hp.TodayPrice, 0) [TodayPrice],
+		COALESCE(hp.PriceSource, '') [PriceSource]
 	FROM
 		DateRange dr
-		CROSS APPLY dbo.tf_GetHistoricalSiteFuelPricesForDay(dr.TheDate, @SiteId) hfp
+		CROSS APPLY FuelTypes ft
+		LEFT JOIN HistoricalPrices hp ON hp.FuelTypeId = ft.FuelTypeId AND hp.PriceDate = dr.TheDate
 	ORDER BY
-		dr.TheDate DESC
+		dr.TheDate DESC,
+		CASE ft.FuelTypeId 
+			WHEN 2 THEN 1	-- Unleaded (1st)
+			WHEN 6 THEN 2	-- Diesel (2nd)
+			WHEN 1 THEN 3	-- Super-Unleaded (3rd)
+		END ASC
 
 RETURN 0
