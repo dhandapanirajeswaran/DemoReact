81553754b8c66a253e0dbc37c5de3045cf25ec73
garry-leeder <garry.leeder@sainsburys.co.uk>
2017-10-16 09:20:34 +0100

    updated batch files for /bin/debug folder

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.WinService/Uninstall.bat
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.WinService/Uninstall.bat
@@ -1,4 +1,4 @@
 
 ECHO --- PETROL PRICING WINDOWS SERVICE UN-INSTALLER --
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /u "JsPlc.Ssc.PetrolPricing.WinService.exe"
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /u "bin\Debug\JsPlc.Ssc.PetrolPricing.WinService.exe"
