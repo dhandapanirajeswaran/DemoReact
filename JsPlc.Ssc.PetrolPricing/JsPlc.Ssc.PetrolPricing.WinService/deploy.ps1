8e264df0a068941c224e0dad4d305086635aaac8
Sandip Vaidya <sandip.vaidya@sainsburys.co.uk>
2017-10-30 16:44:19 +0000

    added deploy.ps1

---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.WinService/deploy.ps1
@@ -0,0 +1,16 @@
+$configFile = "$env:APPLICATION_PATH\JsPlc.Ssc.PetrolPricing.WinService.exe.config"
+[xml] $xmlfile = Get-Content $configFile
+write-host "Transforming the config file" $configFile
+foreach ($item in $xmlfile.configuration.appSettings.add){
+  if($item.key -eq "ServicesBaseUrl"){
+    write-host "Updating the app settings for " $item.key " to " $env:ServicesBaseUrl
+    $item.value = $env:ServicesBaseUrl
+  } 
+}
+$xmlfile.Save( (Resolve-Path($configFile)))
+write-host "Transformation completed for App.config."
+
+#Delete the Windows Service log if it exists and then recreate it
+
+remove-eventlog "OMR Windows Service"
+new-eventlog -source "OMR Windows Service" -logname "OMR Windows Service"
