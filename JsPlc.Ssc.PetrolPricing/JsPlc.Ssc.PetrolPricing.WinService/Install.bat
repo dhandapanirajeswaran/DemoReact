d9e04810be0e220abe6f3f972f9450346239c97b
garry-leeder <garry.leeder@sainsburys.co.uk>
2017-10-30 10:33:25 +0000

    FC-322 added "net start/stop" to Window Service batch files

--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.WinService/Install.bat
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.WinService/Install.bat
@@ -1,3 +1,5 @@
 ECHO --- PETROL PRICING WINDOWS SERVICE INSTALLER --
 
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe "JsPlc.Ssc.PetrolPricing.WinService.exe"
+
+net start PetrolPricingWinService
--- JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.WinService/Uninstall.bat
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.WinService/Uninstall.bat
@@ -1,4 +1,6 @@
 
 ECHO --- PETROL PRICING WINDOWS SERVICE UN-INSTALLER --
 
+net stop PetrolPricingWinService
+
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /u "JsPlc.Ssc.PetrolPricing.WinService.exe"
