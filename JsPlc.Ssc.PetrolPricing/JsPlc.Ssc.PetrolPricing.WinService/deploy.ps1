b837182c709e76fa90f0e8294c091078c6ca17b2
garry-leeder <garry.leeder@sainsburys.co.uk>
2017-10-31 09:34:24 +0000

    FC-322 added deploy.ps1 to WinService project.

---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.WinService/deploy.ps1
@@ -0,0 +1,17 @@
+﻿$configFile = "$env:APPLICATION_PATH\JsPlc.Ssc.PetrolPricing.WinService.exe.config"
+[xml] $xmlfile = Get-Content $configFile
+write-host "Transforming the config file" $configFile
+foreach ($item in $xmlfile.configuration.appSettings.add){
+  if($item.key -eq "ServicesBaseUrl"){
+    write-host "Updating the app settings for " $item.key " to " $env:ServicesBaseUrl
+    $item.value = $env:ServicesBaseUrl
+  } 
+}
+$xmlfile.Save( (Resolve-Path($configFile)))
+write-host "Transformation completed for App.config."
+
+#Delete the Windows Service log if it exists and then recreate it
+
+remove-eventlog "PetrolPricingWinService"
+new-eventlog -source "PetrolPricingWinService" -logname "PetrolPricingWinService"
+
