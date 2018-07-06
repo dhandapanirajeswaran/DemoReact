956b46c4a3258273bf0c420a18837a5ac2b8aed5
Aaron Constance <aaron.constance@sainsburys.co.uk>
2018-07-06 10:10:59 +0100

    INC1001681 Stop throwing exception when PriceMovementReport filename prefixed with anything other than Sainsburys

---
+++ JsPlc.Ssc.PetrolPricing/JsPlc.Ssc.PetrolPricing.Core/ExportReportFileTypeLookup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JsPlc.Ssc.PetrolPricing.Core
+{
+    public class ExportReportFileTypeLookup
+    {
+        private static readonly Dictionary<string, ReportExportFileType> ReportFilenameToFileTypeMap = new Dictionary<string, ReportExportFileType>()
+        {
+            {"CompetitorSites", ReportExportFileType.CompetitorSites},
+            {"CompetitorsPriceRange", ReportExportFileType.CompetitorsPriceRange},
+            {"CompetitorsPriceRangeByCompany", ReportExportFileType.CompetitorsPriceRangeByCompany},
+            {"NationalAverageReport", ReportExportFileType.NationalAverageReport},
+            {"NationalAverageReport2", ReportExportFileType.NationalAverageReport2},
+            {"PriceMovementReport", ReportExportFileType.PriceMovementReport},
+            {"PricePointsReport", ReportExportFileType.PricePointsReport},
+            {"QuarterlySiteAnalysisReport", ReportExportFileType.QuarterlySiteAnalysis },
+            {"LastSitePrices" , ReportExportFileType.LastSitePrices},
+            {"Compliance", ReportExportFileType.Compliance }
+        };
+
+        public ReportExportFileType GetReportType(string fileName)
+        {
+            if (ReportFilenameToFileTypeMap.ContainsKey(fileName))
+            {
+                return ReportFilenameToFileTypeMap[fileName];
+            }
+            else if (fileName.EndsWith("PriceMovementReport"))
+            {
+                return ReportExportFileType.PriceMovementReport;
+            }
+            else
+            {
+                throw new ArgumentException($"Unknown report type for filename: {fileName}");
+            }
+        }
+    }
+}
